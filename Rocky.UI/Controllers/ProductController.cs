a8f5420e9e2abe83516a95c624561d4ee5932b58
Beniamin <beniamin@eklos.ro>
2026-03-06 10:47:02 +0200

    ~ start with 41 video

--- Rocky.UI/Controllers/ProductController.cs
+++ Rocky.UI/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Rocky.UI.Data;
 using Rocky.UI.Models;
@@ -18,51 +19,37 @@ namespace Rocky.UI.Controllers
         public async Task<IActionResult> Index()
         {
             IEnumerable<Product> products = await _db.Products
-                .Include(nameof(Product))
+                .Include(nameof(Category))
                 .ToListAsync();
 
             return View(products);
         }
 
-        public IActionResult Create()
+        public IActionResult Save(int? id)
         {
-            return View();
-        }
+            IEnumerable<SelectListItem> categoryDropdown = _db.Categories.Select(x => new SelectListItem(x.Name, x.Id.ToString()));
+            ViewBag.CategoryDropdown = categoryDropdown;
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Product product)
-        {
-            if (!ModelState.IsValid)
-                return View(product);
-
-            await _db.Products.AddAsync(product);
-            await _db.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || id == 0)
-                return NotFound();
-
-            var product = await _db.Products.FirstOrDefaultAsync(x => x.Id == id);
-            if (product is null)
-                return NotFound();
+            Product product = new();
+            if (id is not null)
+            {
+                product = _db.Products.FirstOrDefault(product => product.Id == id);
+                if (product is null)
+                    return NotFound();
+            }
 
             return View(product);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Product product)
+        public async Task<IActionResult> Save(Product product)
         {
             if (!ModelState.IsValid)
                 return View(product);
 
-            _db.Products.Update(product);
+            await _db.Products.AddAsync(product);
             await _db.SaveChangesAsync();
-
             return RedirectToAction(nameof(Index));
         }
 
